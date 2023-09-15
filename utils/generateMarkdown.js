37440d0c644132c97773eb4bc4fd32ab72faa4f4
AztekAsylum <mateo.rodriguez85@gmail.com>
2023-09-15 15:41:11 -0600

    updated license sections

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,82 +4,77 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "MIT") {
+    return "[MIT link](https://choosealicense.com/licenses/mit/)";
+  }
+  if (license === "GNU") {
+    return "[GNU link](https://choosealicense.com/licenses/gpl-3.0/)";
+  }
+  if (license === "GNU GPLv3") {
+    return "[GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/)";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === "MIT") {
+    return "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+  }
+  if (license === "GNU") {
+    return "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
+  }
+  if (license === "GNU GPLv3") {
+    return "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+  
   ## Description
   
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+  ${data.description}
   
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
-  
-  ## Table of Contents (Optional)
-  
-  If your README is long, add a table of contents to make it easy for users to find what they need.
+  ## Table of Contents
   
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
+  - [Contribution](#contribution)
+  - [Test](#test)
   - [License](#license)
+  - [Questions](#questions)
   
   ## Installation
   
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+  ${data.installation}
   
   ## Usage
   
-  Provide instructions and examples for use. Include screenshots as needed.
+  ${data.usage}
   
-  To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+  ## Contribution
   
-      ```md
-      ![alt text](assets/images/screenshot.png)
-      ```
+  ${data.contribution}
   
-  ## Credits
+  ## Test
   
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
+  ${data.test}
   
   ## License
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ${renderLicenseSection(data.license)}
   
-  ---
+  ${renderLicenseLink(data.license)}
   
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  ## Questions
   
-  ## Badges
+  ${data.questions}
   
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-  
-  ## Features
-  
-  If your project has a lot of features, list them here.
-  
-  ## How to Contribute
-  
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-  
-  ## Tests
-  
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-
 `;
 }
 
